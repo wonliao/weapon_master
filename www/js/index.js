a91af151a9b7a83a423e13c36d14c9fc7dc33a3a
wonliao <wonliao@941wan.com.tw>
2013-11-25 22:34:45 +0800

    新增 5個基本頁面

---
+++ www/js/index.js
@@ -0,0 +1,69 @@
+var app = {
+
+	isMobile: false,	// 是否為 Mobile 裝置
+
+    initialize: function() {
+		
+		this.isMobile = (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));
+
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+
+		if(this.isMobile == true)	document.addEventListener('deviceready', this.onDeviceReady, false);
+    	else						this.onDeviceReady();
+	},
+
+    onDeviceReady: function() {
+
+		app.receivedEvent('deviceready');
+    },
+
+    receivedEvent: function(id) {
+
+		// 初始化
+		this.init();
+	},
+	
+	// 初始化
+	init: function() {
+
+		// 設定按鈕
+		this.setButton();
+		
+		// 預設開啟 採礦 頁
+		this.gotoPage("mining");
+    },
+	
+	// 設定按鈕
+	setButton: function() {
+
+		$('#mining_btn').click(		function() {	app.gotoPage("mining");		});	// 採礦
+		$('#forging_btn').click(	function() {	app.gotoPage("forging");	});	// 鍛造
+		$('#battle_btn').click(		function() {	app.gotoPage("battle");		});	// 戰鬥
+		$('#equipment_btn').click(	function() {	app.gotoPage("equipment");	});	// 裝備
+		$('#shop_btn').click(		function() {	app.gotoPage("shop");		});	// 商店
+	},
+	
+	// 轉跳頁面
+	gotoPage: function(pageName) {
+
+		// 清空全部頁面
+		$("#mining_div").empty();
+		$("#forging_div").empty();
+		$("#battle_div").empty();
+		$("#equipment_div").empty();
+		$("#shop_div").empty();
+
+		var fileName = "./pages/" + pageName + ".html";
+		var output = "#" + pageName + "_div";
+
+		// post 頁面
+		$.post( fileName,
+			function( data ) {
+				$(output).html( data );
+			}
+		);
+	}
+};
