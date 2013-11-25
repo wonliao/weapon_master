4517d9a95eeefbc82d17f6575595a413e260046a
wonliao <wonliao@941wan.com.tw>
2013-11-26 00:19:21 +0800

    調整 採礦頁版面

--- www/js/mining.js
+++ www/js/mining.js
@@ -1,1 +1,45 @@
-// JavaScript Document
+var mining = {
+
+    initialize: function() {
+		
+		// 設定按鈕
+		this.setButton();
+	
+		
+		// 產礦迴圈
+		this.producingMinesLoop();
+
+    },
+
+	// 設定按鈕
+	setButton: function() {
+		
+		
+	},
+
+	// 產礦迴圈
+	producingMinesLoop: function() {
+		
+		var d = new Date();
+		var sec = d.getTime();
+		
+		
+		var lastTime = window.localStorage.getItem("MINING_LAST_TIME");
+		
+		var diff = sec - lastTime; 
+		console.log("sec("+sec+")");
+		console.log("lastTime("+lastTime+")");
+		console.log("diff("+diff+")");
+		
+		window.localStorage.setItem("MINING_LAST_TIME", sec);
+		
+	},
+	
+	// 顯示畫面
+	render: function() {
+		
+	
+	}
+	
+};
+
