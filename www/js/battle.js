e36998e53afb7cefd5b5d4d0e8519c762083a270
wonliao <wonliao@941wan.com.tw>
2013-11-28 00:18:56 +0800

    1、新增 戰鬥頁加入拖拉武器進戰鬥畫面 2、修正 鍛造頁的武器數量 css

--- www/js/battle.js
+++ www/js/battle.js
@@ -1,1 +1,70 @@
-// JavaScript Document
+var battle = {
+
+	// 是否為 Mobile 裝置
+	isMobile: false,
+
+	// 初始化
+    initialize: function() {
+
+		this.isMobile = (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));
+
+		// 設定拖拉功能
+		this.setDragAndDrop();
+
+		// 設定按鈕
+		this.setButton();
+
+		// 更新抬頭訊息
+		this.showHUD();
+    },
+
+	// 設定拖拉功能
+	setDragAndDrop: function() {
+		
+		$('.weapon').draggable({ revert: true, revertDuration:0, helper: "clone"});
+
+		$( "#playground" ).droppable({
+		  drop: function( event, ui ) {
+			 
+			 console.log("droped");
+			
+		  }
+		});
+	},
+
+	// 設定按鈕
+	setButton: function() {
+
+		
+	},
+
+	// 更新抬頭訊息
+	showHUD: function() {
+
+		// 武器數量
+		$('#createNum1').text(player.weapon1);
+		$('#createNum2').text(player.weapon2);
+		$('#createNum3').text(player.weapon3);
+		$('#createNum4').text(player.weapon4);
+	}
+};
+
+function Click(object, callback) {
+
+	// Mobile 裝置
+	if(forging.isMobile == true) {
+
+		object.bind('touchstart', function() {
+
+			callback(object);
+		});
+	// 網頁版	
+	} else {
+
+		object.click(function() {
+
+			callback(object);
+		});
+	}
+}
+
